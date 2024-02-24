49d753778c866600e904d36c1fc59689157386a2
Pat Reilly <11032237+patreilly@users.noreply.github.com>
2024-02-24 10:28:56 -0800

    Update parse_ics.py

--- parse_ics/parse_ics.py
+++ parse_ics/parse_ics.py
@@ -1,10 +1,14 @@
-# ics to csv example
-# dependency: https://pypi.org/project/vobject/
+# takes a team sideline subscription URL and converts it to a team snap .csv
 
 import vobject
 import csv
 import pytz
 import datetime as dt
+import requests as r
+
+TIMEZONE_NAME = 'America/Los_Angeles'
+TEAMSIDELINE_URL = 'http://tmsdln.com/28knr'
+TEAM_NAME = 'Due Soccer Club (Maroon)'
 
 def convert_from_utc(utc_dt, timezone_name='America/Los_Angeles'):
     """
@@ -20,13 +24,19 @@ def convert_from_utc(utc_dt, timezone_name='America/Los_Angeles'):
     # Convert the UTC datetime to the current time zone
     local_dt = utc_dt.replace(tzinfo=pytz.utc).astimezone(tz)
 
-
-
     # Format the datetime as a string
     return local_dt
 
 
 def get_opponent(description, my_team):
+    """
+    takes a matchup description and returns the opponent name
+    :description    : Teams Sideline matchup description
+    :my_team        : my team name
+    :returns        : opponent name for a given matchup
+    """
+
+    my_team = my_team.strip()
     opponent = ""
     if " vs. " in description:
         teams = description.split(" vs. ")
@@ -38,7 +48,14 @@ def get_opponent(description, my_team):
 
 
 def get_home_away(description, my_team):
+    """
+    takes a matchup description and returns the home or away
+    :description    : Teams Sideline matchup description
+    :my_team        : my team name
+    :returns        : home or away for a given matchup.  "h" for home, "a" for away.  "" if not found. 
+    """
     home_away = ""
+    my_team = my_team.strip()
     if " vs. " in description:
         teams = description.split(" vs. ")
         if teams[0].strip() == my_team:
@@ -52,22 +69,20 @@ with open('teamsnap_schedule.csv', mode='w') as csv_out:
     csv_writer = csv.writer(csv_out, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     header_column_names = ['Date','Time','Duration (HH:MM)','Name','Opponent Name','Location Name', 'Home or Away']
     csv_writer.writerow(header_column_names)
-    my_team = 'Soccer Team' # whatever is used on gssl schedule
 
-    # read the data from the file
-    data = open("Schedule.ics").read()
+    schedule = r.get(TEAMSIDELINE_URL).text
 
     # iterate through the contents
-    for cal in vobject.readComponents(data):
+    for cal in vobject.readComponents(schedule):
         for component in cal.components():
             if component.name == "VEVENT":
-                event_local_tz = convert_from_utc(component.dtstart.value)
+                event_local_tz = convert_from_utc(utc_dt=component.dtstart.value, timezone_name=TIMEZONE_NAME)
                 csv_writer.writerow([
                     event_local_tz.strftime("%m/%d/%Y"), # Date
                     event_local_tz.strftime("%I:%M %p"), # Time
                     '1:30', # duration
                     'Game', # Name
-                    get_opponent(component.summary.value, my_team), # opponent name
+                    get_opponent(component.summary.value, TEAM_NAME), # opponent name
                     component.location.value, # location name
-                    get_home_away(component.summary.value, my_team) # home or away
+                    get_home_away(component.summary.value, TEAM_NAME) # home or away
                     ])
